06a5adc86e46d82e12270f9c8c246c6e1467c28c
bilaluslu <uslubilal45@gmail.com>
2020-08-24 11:21:31 -0400

    cok seker

--- src/test/java/Cay/Seker.java
+++ src/test/java/Cay/Seker.java
@@ -5,6 +5,7 @@ public class Seker {
     public static void main(String[] args) {
 
         System.out.println("Tek sekerli ceeeeeeek");
+        System.out.println("Cok sekerli ceeeeeeek");
 
     }
 }
